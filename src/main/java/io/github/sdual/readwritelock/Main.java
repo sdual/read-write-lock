b7482bccd62ca07d3193eee71740e913ec472184
sdual <shr.nrht.0623@gmail.com>
2018-11-02 19:15:52 +0900

    add docs

---
+++ src/main/java/io/github/sdual/readwritelock/Main.java
@@ -0,0 +1,11 @@
+package io.github.sdual.readwritelock;
+
+import java.util.concurrent.locks.StampedLock;
+
+public class Main {
+
+  public static void main(String[] args) {
+    StampedLock lock = new StampedLock();
+  }
+
+}
