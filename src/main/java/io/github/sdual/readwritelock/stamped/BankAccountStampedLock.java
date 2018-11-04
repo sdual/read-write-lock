07751bfcdebd80f1b0c3de38eb3407741da780a9
sdual <shr.nrht.0623@gmail.com>
2018-11-05 03:28:33 +0900

    stamped lock

---
+++ src/main/java/io/github/sdual/readwritelock/stamped/BankAccountStampedLock.java
@@ -0,0 +1,46 @@
+package io.github.sdual.readwritelock.stamped;
+
+import java.util.concurrent.locks.StampedLock;
+
+public class BankAccountStampedLock {
+
+  private final StampedLock sl = new StampedLock();
+  private long balance;
+
+  public BankAccountStampedLock(long balance) {
+    this.balance = balance;
+  }
+
+  public void deposit(long amount) {
+    long stamp = sl.writeLock();
+    try {
+      balance += amount;
+    } finally {
+      sl.unlockWrite(stamp);
+    }
+  }
+
+  public void withdraw(long amount) {
+    long stamp = sl.writeLock();
+    try {
+      balance -= amount;
+    } finally {
+      sl.unlockWrite(stamp);
+    }
+  }
+
+  public long getBalanceOptimisticRead() {
+    long stamp = sl.tryOptimisticRead();
+    long balance = this.balance;
+    if (!sl.validate(stamp)) {
+      stamp = sl.readLock();
+      try {
+        balance = this.balance;
+      } finally {
+        sl.unlockRead(stamp);
+      }
+    }
+    return balance;
+  }
+
+}
